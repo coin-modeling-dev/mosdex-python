3cdb3a8fbed1b88b3965892c7f7c51fea47c7e0a
Alan J. King <kingaj@us.ibm.com>
2020-03-19 18:54:19 -0400

    print out the high level members of MOSDEX

--- src/read-mosdex.py
+++ src/read-mosdex.py
@@ -3,24 +3,48 @@ import os
 from jsonschema import Draft7Validator
 import pprint
 
-example_dir = "../MOSDEX-Examples/MOSDEX-1.2"
+schema_dir = "data"
+file_dir = "data"
 
-cutting_stock_file = "cuttingStock_1-2.json"
-with open(os.path.join(example_dir, cutting_stock_file)) as f:
+file = "cuttingStock_1-2.json"
+with open(os.path.join(file_dir, file)) as f:
     cs_json = json.load(f)
 
-schema_file = "MOSDEXSchemaV1-2.json"
-with open(os.path.join(example_dir, schema_file)) as f:
+schema_file = "MOSDEXSchemaV1-2-ajk.json"
+with open(os.path.join(schema_dir, schema_file)) as f:
     mosdex_schema = json.load(f)
 
 pp = pprint.PrettyPrinter(indent=8)
 
 
-v = Draft7Validator(mosdex_schema)
-print("Is the file {} a valid instance of {}: {}".format(cutting_stock_file, schema_file, v.is_valid(cs_json)))
-print()
+def is_it_valid():
+    v1 = Draft7Validator(mosdex_schema)
+    print("Is the file {} a valid instance of {}: {}".format(file, schema_file, v1.is_valid(cs_json)))
+    print()
 
-for error in sorted(v.iter_errors(cs_json), key=str):
-    print()
-    pp.pprint(error.message)
-    print()
+    for error in sorted(v1.iter_errors(cs_json), key=str):
+        print()
+        pp.pprint(error.message)
+        print()
+
+
+static_members = ["SYNTAX", "CLASS", "HEADING"]
+
+
+def mosdex_members(mosdex_problem: dict, do_print=False):
+    mosdex_classes = set([mosdex_problem[k]["CLASS"] for k in mosdex_problem
+                          if k not in static_members])
+    members = {}
+    for m in mosdex_classes:
+        members[m] = [k for k in mosdex_problem
+                      if k not in static_members and mosdex_problem[k]["CLASS"] == m]
+        if do_print:
+            print("{:12s} {}".format(m, members[m]))
+    return members
+
+
+# print out the high level members of the mosdex problem
+problems = mosdex_members(cs_json, do_print=True)
+for p in problems["PROBLEM"]:
+    print("\n{}".format(p))
+    mosdex_members(cs_json[p], do_print=True)
