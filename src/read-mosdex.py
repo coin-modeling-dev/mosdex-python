edef77d314cf9222423a4b13d7dea69818561bd2
Alan J. King <kingaj@us.ibm.com>
2020-05-02 17:17:50 -0400

    initial coding for creating tables in database

--- src/read-mosdex.py
+++ src/read-mosdex.py
@@ -3,6 +3,54 @@ import os
 from jsonschema import Draft7Validator
 import pprint
 
+import sqlite3
+from sqlite3 import Error
+
+import records
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def create_module(conn, module_name):
+    """
+    Create a new project into the projects table
+    :param conn:
+    :param project:
+    :return: project id
+    """
+    sql = ''' INSERT INTO modules(module_name)
+              VALUES(?) '''
+    cur = conn.cursor()
+    cur.execute(sql, module_name)
+    return cur.lastrowid
+
 
 def mosdex_open_and_test(problem_file, schema_file, do_print=False):
     with open(problem_file) as f:
@@ -84,23 +132,112 @@ if __name__ == "__main__":
 
     # Print out the data elements
     for k, m in cs_data.items():
+        print("\n\n***{}***".format(k))
         for k1, m1 in m.items():
             mosdex_data_io = m1["TYPE"]
             if "RECIPE" in m1:
-                print("{}.{}.{}:".format(k, k1, mosdex_data_io))
-                if "DEPENDS" in m1:
-                    print("\t{}\t{}".format("DEPENDS", m1["DEPENDS"]))
-                print("\t{}".format("RECIPE"))
+                print("\t{}.{}.{}:".format(k, k1, mosdex_data_io))
                 for step in m1["RECIPE"]:
                     print("\t\t{}".format(step))
             if "SINGLETON" in m1:
-                print("{}.{}.{}:".format(k, k1, mosdex_data_io))
-                print("\t{} {}".format("SINGLETON", m1["SINGLETON"]))
+                print("\t{}.{}.{}:".format(k, k1, mosdex_data_io))
+                print("\t\t{}: {}".format("SINGLETON", m1["SINGLETON"]))
             if "SCHEMA" in m1:
-                print("{}.{}.{}:".format(k, k1, mosdex_data_io))
-                print("\t{} {}".format("SCHEMA", m1["SCHEMA"]))
+                print("\t{}.{}.{}:".format(k, k1, mosdex_data_io))
+                print("\t\t{}: {}".format("SCHEMA", m1["SCHEMA"]))
                 if "INSTANCE" in m1:
-                    print("\t{} {}".format("INSTANCE", m1["INSTANCE"]))
+                    print("\t\t{}: {}".format("INSTANCE", m1["INSTANCE"]))
+
+    db = records.Database('sqlite://')
+
+    # Create modules table
+    db.query('DROP TABLE IF EXISTS modules_table')
+    db.query('CREATE TABLE modules_table ( module_id integer KEY, module_name text, item_name text, '
+             'table_name text, recipe_name text, '
+             'CONSTRAINT module_item_pair PRIMARY KEY (module_name, item_name)) ')
+
+    # Create the singletons table
+    db.query('DROP TABLE IF EXISTS singletons_table')
+    db.query('CREATE TABLE singletons_table ( singleton_id integer KEY, '
+             'module_name text, item_name text, singleton_name text, '
+             's_value number, s_recipe text)')
+
+    print("\n\n***Process the modules***")
+    # Process the modules
+    modules_id = 0
+    singletons_id = 0
+    for k, m in cs_modules.items():
+
+        # add the data items
+        data = mosdex_members(m)["DATA"]
+        for k1, m1 in data.items():
+            if "SINGLETON" in m1:
+                recipe_name = None
+                db.query('INSERT INTO modules_table (module_id, module_name, item_name, table_name, recipe_name) '
+                         'VALUES(:mid, :mname, :iname, :tname, :rname)',
+                         mid=modules_id, mname=k, iname=k1, tname="singletons_table", rname=recipe_name)
+                modules_id += 1
+                print("\t{}.{}:".format(k, k1))
+                for s_name, s_value in m1["SINGLETON"].items():
+                    r_value = None
+                    if type(s_value) is str:
+                        r_value = s_value
+                        s_value = None
+                    db.query('INSERT INTO singletons_table (singleton_id, singleton_name, '
+                             's_value, s_recipe, module_name, item_name ) '
+                             'VALUES(:s_id, :n, :v, :r, :m , :i) ',
+                             s_id=singletons_id, n=s_name, v=s_value, r=r_value, m=k, i=k1)
+                    singletons_id += 1
+            if "SCHEMA" in m1:
+                db.query('INSERT INTO modules_table (module_name, item_name, table_name, recipe_name)'
+                         'VALUES(:mname, :iname, :tname, :rname)',
+                         mname=k, iname=k1, tname=k1, rname=None)
+                sql_string = 'CREATE TABLE ' + k1
+                sep_string = ' ('
+                for col_name, col_type in m1["SCHEMA"].items():
+                    sql_string = sql_string + sep_string + col_name + ' ' + col_type
+                    if sep_string is ' (':
+                        sep_string = ', '
+                sql_string = sql_string + ' )'
+                print("\t{}".format(sql_string))
+
+    print("\n***List the Tables***")
+    rows = db.query('SELECT * FROM modules_table')
+    print(rows.export('csv'))
+    rows = db.query('SELECT * FROM singletons_table')
+    print(rows.export('csv'))
 
 
+'''
+    # Use in-memory database
+    database = ':memory:'
+
+    # create a database connection
+    conn = create_connection(database)
+    assert conn is not None
+
+    # create the modules table
+    sql_create_modules_table = """ CREATE TABLE IF NOT EXISTS modules (
+                                        module_name text NOT NULL PRIMARY KEY,
+                                    ); """
+    create_table(conn, sql_create_modules_table)
+
+    # add the module names to the modules table
+    for k in cs_modules.keys():
+        create_module(k)
+
+
+
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );"""
+
+'''
 
