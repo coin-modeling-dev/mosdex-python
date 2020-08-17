13aacf43345ceb6322c614db2fb3eaf2ddfc6f66
Alan J. King <kingaj@us.ibm.com>
2020-08-17 15:20:08 -0400

    revising setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='mosdex-python',
     version='2020.1-dev1',
-    packages=['mosdex'],
+    packages=find_packages(exclude="tests"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/coin-modeling-dev/mosdex-python.git',
-    license='MIT',
     author='Alan King',
     author_email='kingaj@us.ibm.com',
-    description='python package for running initial demo of MOSDEX standard for modular optimization problems'
+    description='Package for running demo of MOSDEX modular optimization problem data standard',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
 )
