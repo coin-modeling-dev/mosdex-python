288eb5ae5aac1da6818f52240ab9a6375d6bd516
Alan J. King <kingaj@us.ibm.com>
2020-08-17 15:44:01 -0400

    revising setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mosdex-python',
-    version='2020.1-dev1',
+    version='2020.1-dev.1',
     packages=find_packages(exclude="tests"),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/coin-modeling-dev/mosdex-python.git',
+    url='https://github.com/coin-modeling-dev/mosdex-python/archive/v2020.1-dev.1.tar.gz',
     author='Alan King',
     author_email='kingaj@us.ibm.com',
     description='Package for running demo of MOSDEX modular optimization problem data standard',
@@ -18,5 +18,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    install_requires=['pandas', 'json', 'pprint', 'jsonschema', 'records']
 )
