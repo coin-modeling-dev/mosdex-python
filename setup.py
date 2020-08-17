b62545a5a84b9291eb6a654321507ee6758c3002
Alan J. King <kingaj@us.ibm.com>
2020-08-17 16:41:55 -0400

    revising setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['pandas', 'json', 'pprint', 'jsonschema', 'records']
+    install_requires=['pandas', 'pprint', 'jsonschema', 'records']
 )
